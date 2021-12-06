dd16cd75c8412d778b91a8c1210e2b2eb915ff36
leesoomin <leesoomin.wat@gmail.com>
2021-12-06 11:23:37 +0900

    파이썬 예제 06

--- study01/tk05.py
+++ study01/tk05.py
@@ -9,6 +9,6 @@ b4 = Button(window, text="박스 #4", bg="pink", fg="white")
 b1.grid(row=0, column=0)
 b2.grid(row=0, column=1)
 b3.grid(row=1, column=0)
-b4.grid(row=1, column=1)
+b4.grid(row=1, column=1) #b1~b4-버튼이라는 객체, grid=액셀처럼 사용하는 것, row-행, column-칸
 
 window.mainloop()
